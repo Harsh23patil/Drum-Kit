1a79f7aea26caa253a03a2517564771414a1f227
Harshal Patil <86585860+Harsh23patil@users.noreply.github.com>
2022-06-02 00:27:00 +0530

    changing img location

--- index.js
+++ index.js
@@ -18,25 +18,25 @@ document.addEventListener("keydown",function(event){
 });
 function makeSound(key) {
     if(key === "w" ){
-        var audio = new Audio("Additional Files/sounds/tom-1.mp3");
+        var audio = new Audio("tom-1.mp3");
         audio.play();
     }else if(key === "a"){
-        var audio = new Audio("Additional Files/sounds/tom-2.mp3");
+        var audio = new Audio("tom-2.mp3");
         audio.play();
     }else if(key === "s"){
-        var audio = new Audio("Additional Files/sounds/tom-3.mp3");
+        var audio = new Audio("tom-3.mp3");
         audio.play();
     }else if(key === "d"){
-        var audio = new Audio("Additional Files/sounds/tom-4.mp3");
+        var audio = new Audio("tom-4.mp3");
         audio.play();
     }else if(key === "j"){
-        var audio = new Audio("Additional Files/sounds/snare.mp3");
+        var audio = new Audio("snare.mp3");
         audio.play();
     }else if(key === "k"){
-        var audio = new Audio("Additional Files/sounds/crash.mp3");
+        var audio = new Audio("crash.mp3");
         audio.play();
     }else if(key ==="l"){
-        var audio = new Audio("Additional Files/sounds/kick-bass.mp3");
+        var audio = new Audio("kick-bass.mp3");
         audio.play();
     }else{
         console.log(key);
